0e4143879a7fb4c3a9a55d450faf3e2195112866
Mohammad Mujahid Nadeem <59459907+mujahidnadeem6852443@users.noreply.github.com>
2025-05-06 20:41:07 +0500

    Jwt only

--- src/main/java/com/security_project/securityproject/repository/HeroesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.security_project.securityproject.repository;
-
-import com.security_project.securityproject.model.Heroes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HeroesRepository extends JpaRepository<Heroes, Integer> {
-
-}
--- src/main/java/com/security_project/securityproject/service/HeroService.java
+++
@@ -1,9 +0,0 @@
-package com.security_project.securityproject.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class HeroService {
-
-
-}
